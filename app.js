be0854939419bd61a2c8303c8b982f038a44520a
shaiksaidabi <sk.saidabi0203@gmail.com>
2025-06-15 19:22:06 +0530

    changes

--- app.js
+++ app.js
@@ -50,5 +50,5 @@ io.on('connection', (socket) => {
 
 // Start the server
 server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+  console.log('Server running on http://localhost:8080');
 });
